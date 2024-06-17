694f67e54247a3816c28049a7e441f01387a5163
shijia.yuan <shijia.yuan@shopee.com>
2024-06-17 16:52:57 +0800

    init

--- blog.config.js
+++ blog.config.js
@@ -27,7 +27,7 @@ const BLOG = {
   previewImagesEnabled: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateHost: 'og-zl.vercel.app', // The link to generate OG image, don't end with a slash
-  defaultCover: '/cover.jpg',
+  defaultCover: '/favicon.png',
   socialLink: {
     github: 'https://github.com/shij-yuan',
     telegram: 'https://t.me/sherlockied'
